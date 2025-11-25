570e7c12030caa5bf9b73c1000593df149f3b046
TimKhami <timfarma95@gmail.com>
2025-11-26 00:15:40 +0300

    V1

---
+++ src/main/java/com/example/library/modelEntity/Book.java
@@ -0,0 +1,40 @@
+package com.example.library.modelEntity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    //Идентификатор
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private int id;
+    //Название
+    @Column(nullable = false)
+    private String title;
+    //Жанр
+    @Column(nullable = false)
+    private String genre;
+    //Автор
+    @Column(nullable = false)
+    private String author;
+    //Издательство
+    @Column(nullable = false)
+    private String publisher;
+    //Год издания
+    @Column(nullable = false)
+    private LocalDate publishedDate;
+    //Место издания
+    @Column(nullable = false)
+    private String placeOfPublication;
+
+
+
+}
---
+++ src/main/java/com/example/library/modelEntity/User.java
@@ -0,0 +1,12 @@
+package com.example.library.modelEntity;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private LocalDate dateOfBirth;
+    private String gender;
+}
